0017c1d55abca076cc9c3d38e8ae3cc9c69f2816
OluwatisheElesi <tisheelesh.logic@gmail.com>
2022-10-04 15:31:21 -0400

    1

---
+++ Projects/Project1/src/components/chessboard.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+import './chessboard.css';
+
+
+
+// chessboard layout
+
+
+
+
+
+
+
+
+
+
+
+
+
+export default function Chessboard() {
+    return <div>Hello!</div>
+}
